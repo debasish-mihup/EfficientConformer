da3dc36b5d2f3c59289834ce670b641167264cb0
Debasish Mitra <debasish@mihup.com>
2021-10-24 12:58:49 +0530

    Initial changes

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -108,6 +108,7 @@ def prepare_dataset(training_params, tokenizer_params, tokenizer):
             label = torch.LongTensor(tokenizer.encode(sentence))
             torch.save(label, label_path)
             audio_path_without_extension = ".".join(label_path.split(".")[:-1])
+            print("DEBUG4: " , audio_path_without_extension)
             # Save Audio length
             audio_length = torchaudio.load(audio_path_without_extension + ".wav")[0].size(1)
             torch.save(audio_length, audio_path_without_extension + ".wav_len")
