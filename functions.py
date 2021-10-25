aaf1211044c5a42b3d48b3ced9297c529d4da546
Debasish Mitra <debasish@mihup.com>
2021-10-26 02:37:06 +0530

    Initial changes

--- functions.py
+++ functions.py
@@ -89,7 +89,7 @@ def load_datasets(training_params, tokenizer_params, args):
         "Mihup": {
             "class": MihupDataset,
             "split": {
-                "training": "train"
+                "training": "cheap1"
             }
         }
     }
